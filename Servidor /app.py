b2d009d2bff57741c9f9c6abd4cffacd4d1c3226
maryllusco <maryluzllusco03@gmail.com>
2025-08-20 10:00:26 -0300

    agregamos abrir y cerrar conexion en la ruta que recibe el POST

--- Servidor /app.py
+++ Servidor /app.py
@@ -1,13 +1,34 @@
 from flask import Flask
+import sqlite3
+from flask import Flask, g, request, jsonify
+
+
+def dict_factory(cursor, row):
+    """Arma un diccionario con los valores de la fila."""
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
+
+
+def abrirConexion():
+    if 'db' not in g:
+        g.db = sqlite3.connect("valores.sqlite")
+        g.db.row_factory = dict_factory
+    return g.db
+
+def cerrarConexion(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("api/sensor", methods=['POST'])
+@app.route("/api/sensor", methods=['POST'])
 def sensor():
-    
-
+    db = abrirConexion()
+    datos = request.json
+    nombre = datos ["nombre"]
+    valor = datos ["valor"]
+    print (f"nombre del sensor{nombre}, valor: {valor}")
+    cerrarConexion()
+    return "OK"
